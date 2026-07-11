44366f7552acab715a466f05a9d18c69b0a3999d
Sourav Sarkar <souravcviii@gmail.com>
2026-07-12 00:32:46 +0530

    Add production start script

--- schinterface/src/Student/RegisterStudent.jsx
+++ schinterface/src/Student/RegisterStudent.jsx
@@ -27,7 +27,7 @@ function RegisterStudent() {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/auth/registration",
+        "`${import.meta.env.VITE_API_URL}/api/auth/registration`,",
         formData
       );
 
